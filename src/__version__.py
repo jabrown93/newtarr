b673ca099c8599b4438d284548fe44567ee96358
Jared Brown <github@jaredbrown.io>
2026-03-03 14:56:44 -0800

    Bump version to 0.0.4

--- src/__version__.py
+++ src/__version__.py
@@ -5,8 +5,8 @@ This module provides version metadata for the application.
 Import version information from here rather than hardcoding it elsewhere.
 """
 
-__version__ = "0.0.3"
-__version_info__ = (0, 0, 3, "patch")
+__version__ = "0.0.4"
+__version_info__ = (0, 0, 4, "patch")
 __author__ = "jabrown93"
 __license__ = "GPL-3.0"
 __url__ = "https://github.com/jabrown93/newtarr"
